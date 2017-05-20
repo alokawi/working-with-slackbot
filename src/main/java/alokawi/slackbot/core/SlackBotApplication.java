37e265d4ccd6c400bab1d5077ee2ee0be4697434
Alok Kumar <alokawi@gmail.com>
2017-05-20 12:48:43 +0530

    Add SlackBotApplication & SlackBotService classes

---
+++ src/main/java/alokawi/slackbot/core/SlackBotApplication.java
@@ -0,0 +1,20 @@
+package alokawi.slackbot.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "me.ramswaroop.jbot", "alokawi.slackbot.core" })
+public class SlackBotApplication {
+
+	/**
+	 * Entry point of the application. Run this method to start the sample bots,
+	 * but don't forget to add the correct tokens in application.properties
+	 * file.
+	 *
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackBotApplication.class, args);
+	}
+}
